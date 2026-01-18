b64c397ec4134a01f86871196a3061eb9147bd14
NguyenVan-Truong <truongha21062004@gmail.com>
2026-01-18 20:34:02 +0700

    123

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'logo.clearbit.com' },
-      { protocol: 'https', hostname: 'upload.wikimedia.org' },
-      { protocol: 'https', hostname: 'www.gstatic.com' },
-      { protocol: 'https', hostname: 'via.placeholder.com' },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "logo.clearbit.com" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+      { protocol: "https", hostname: "www.gstatic.com" },
+      { protocol: "https", hostname: "via.placeholder.com" },
     ],
   },
   reactStrictMode: true,
@@ -24,6 +24,6 @@ const nextConfig = {
     },
   },
   productionBrowserSourceMaps: false, // 👈 Tắt sourcemap cho frontend
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
